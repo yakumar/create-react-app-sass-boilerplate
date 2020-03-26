cc0a7fdf4b6b6e9e29a2d1b0d719f8bf57d1edcb
kumarya <yakumar_in@yahoo.com>
2020-03-26 13:14:57 +0530

    build file added

---
+++ build/precache-manifest.70fe3b9adc6f0565b59307d45460b1c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deec21bd44603d9b5dcef0c1fe91c75e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d7857e874befa7d72ef",
+    "url": "/static/css/main.a5e2cf2d.chunk.css"
+  },
+  {
+    "revision": "137a8956c4679829538e",
+    "url": "/static/js/2.6ffc86d1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.6ffc86d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d7857e874befa7d72ef",
+    "url": "/static/js/main.e2a7493c.chunk.js"
+  },
+  {
+    "revision": "ec06a9bac8fe98e87989",
+    "url": "/static/js/runtime-main.cdf9a5af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
